d47f8291618c29edb807df5730eab1cc654667d5
andbamp <andreasbampouris@gmail.com>
2021-10-05 22:06:52 +0300

    Add convex hull edge case tests

--- tests/test_convex_hull_from_points.py
+++ tests/test_convex_hull_from_points.py
@@ -8,6 +8,19 @@ def test_convex_hull_from_points():
   poly = Polygon(chull_points)
   assert chull.__str__() == poly.__str__()
 
+def test_convex_hull_from_points_colinear():
+  point_cloud = np.array([[0,0], [1,2], [1,3], [2,1], [2,2], [3,1]])
+  chull = convex_hull_from_points(point_cloud)
+  chull_points = np.array([[0,0], [3,1], [2,2], [1,3]])
+  poly = Polygon(chull_points)
+  assert chull.__str__() == poly.__str__()
+
+def test_convex_hull_from_points_triangle():
+  point_cloud = points = np.array([[0,-1], [2,1], [1,2]])
+  chull = convex_hull_from_points(point_cloud)
+  poly = Polygon(point_cloud)
+  assert chull.__str__() == poly.__str__()
+
 def test_angle_with_horizontal_q1():
   p1 = np.array([0,0])
   p2 = np.array([1,1])
