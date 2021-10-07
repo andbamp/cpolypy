c4e30f902502fbaa2035c2bbd1daa3eab46e5b11
andbamp <andreasbampouris@gmail.com>
2021-10-07 19:27:40 +0300

    Refactor Polygon

--- src/cpolypy/Polygon.py
+++ src/cpolypy/Polygon.py
@@ -1,10 +1,15 @@
+import numpy as np
+
 class Polygon():
   
-  def __init__(self, points_array = None):
-    if points_array is None:
-      self.points = []
+  def __init__(self, vertices = None):
+    if vertices is None:
+      self.vertices = np.empty(shape = 0)
     else:
-      self.points = points_array.tolist()
+      self.vertices = vertices
   
   def __str__(self):
-    return "Polygon composed of points: {}".format(self.points)
+    return "Polygon composed of points: {}".format(self.vertices.tolist())
+  
+  def edges(self):
+    return((vertices[:-1] - vertices[1:]).T)
--- src/cpolypy/point_is_in_polygon.py
+++ src/cpolypy/point_is_in_polygon.py
@@ -2,8 +2,8 @@ from cpolypy.utils import clockwise_turn
 import numpy as np
 
 def point_is_in_polygon(point, cpoly):
-  points = np.array([*cpoly.points, cpoly.points[0]])
-  for i in range(len(cpoly.points)):
+  points = np.array([*cpoly.vertices, cpoly.vertices[0]])
+  for i in range(len(cpoly.vertices)):
     if(clockwise_turn(points[i], points[i+1], point)):
       return False
   return True
