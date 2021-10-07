c9be8ce64a09f60380f83fec1c403d78a4093dfb
andbamp <andreasbampouris@gmail.com>
2021-10-07 22:44:36 +0300

    Implement separating axis theorem

--- src/cpolypy/do_intersect.py
+++ src/cpolypy/do_intersect.py
@@ -1,3 +1,13 @@
-def do_intersect(cpoly_x, cpoly_y):
-  intersection = False
-  return(intersection)
+from cpolypy.utils import perpendicular_unit
+import numpy as np
+
+def do_intersect(cpoly_1, cpoly_2):
+  axes = perpendicular_unit(np.vstack([cpoly_1.edges(), cpoly_2.edges()]))
+  
+  for axis in axes:
+    proj_1 = np.dot(cpoly_1.vertices, axis.T)
+    proj_2 = np.dot(cpoly_2.vertices, axis.T)
+    if(np.min(proj_1) > np.max(proj_2) or np.min(proj_2) > np.max(proj_1)):
+      return False
+  
+  return True
--- tests/test_do_intersect.py
+++ tests/test_do_intersect.py
@@ -18,3 +18,21 @@ def test_do_intersect_in():
   cpoly_2 = Polygon(np.array([[3,1], [3.5,1.5], [2.5,4], [2,2]]))
   intersection = do_intersect(cpoly_1, cpoly_2)
   assert intersection == True
+
+def test_do_intersect_common_pt():
+  cpoly_1 = Polygon(np.array([[1,1], [3,2], [2,3]]))
+  cpoly_2 = Polygon(np.array([[5,1], [4,3], [3,2]]))
+  intersection = do_intersect(cpoly_1, cpoly_2)
+  assert intersection == True
+
+def test_do_intersect_shifted_away():
+  cpoly_1 = Polygon(np.array([[1,1], [3,2], [2,3]]))
+  cpoly_2 = Polygon(np.array([[6,1], [5,3], [4,2]]))
+  intersection = do_intersect(cpoly_1, cpoly_2)
+  assert intersection == False
+
+def test_do_intersect_shifted_close():
+  cpoly_1 = Polygon(np.array([[1,1], [3,2], [2,3]]))
+  cpoly_2 = Polygon(np.array([[4,1], [3,3], [2,2]]))
+  intersection = do_intersect(cpoly_1, cpoly_2)
+  assert intersection == True
