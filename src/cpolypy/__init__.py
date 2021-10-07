bb857cdf3ed984fddc4e42c7ae782cecc3cb4158
andbamp <andreasbampouris@gmail.com>
2021-10-08 00:06:00 +0300

    Remove exposed internal function

--- src/cpolypy/__init__.py
+++ src/cpolypy/__init__.py
@@ -2,4 +2,3 @@ from cpolypy.Polygon import Polygon
 from cpolypy.convex_hull_from_points import convex_hull_from_points
 from cpolypy.point_is_in_polygon import point_is_in_polygon
 from cpolypy.do_intersect import do_intersect
-from cpolypy.utils import angle_with_horizontal
